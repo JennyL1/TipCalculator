863cbef10995c3f6b6cd9c5f64b178e1f36fed29
Amy Li <amy@amys-macbook-pro-3.local>
2018-12-15 13:34:14 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Amy Li on 12/15/18.
+//  Copyright © 2018 Jenny Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
